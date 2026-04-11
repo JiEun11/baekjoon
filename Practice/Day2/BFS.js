e61133601315490603b45180dd8c2bb2e24de195
bella <shdudtnr3939@naver.com>
2026-04-11 11:28:17 +0900

    feat: Day2 BFS

--- Practice/Day2/BFS.js
+++ Practice/Day2/BFS.js
@@ -1,11 +1,11 @@
-function bfs(startNode) {
+function bfs(startNode, graph) {
     const queue = [startNode];
-    const visitedNodes = new Set();
-    visitedNodes.add(startNode);
+    const visitedNodes = new Set([startNode]);
+    const visitOrder = [];
 
     while (queue.length > 0) {
         const currentNode = queue.shift();
-        console.log(currentNode); // 현재 노드 처리
+        visitOrder.push(currentNode);
 
         // 인접 노드를 큐에 추가
         for (const neighbor of graph[currentNode]) {
@@ -15,4 +15,15 @@ function bfs(startNode) {
             }
         }
     }
+
+    return visitOrder;
 }
+
+const graph = {
+    1: [2, 3],
+    2: [1, 4],
+    3: [1, 4],
+    4: [2, 3],
+}
+
+console.log(bfs(1, graph)); // [1, 2, 3, 4]
